be34558c1119881a2afad613c955b5c3e43e5da2
CodeAldi <codealdi@gmail.com>
2025-02-13 14:34:17 +0700

    ubah tampilan home, footer, dan navbar

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     function index() {
-        return view('home')->with('title','Home');
+        $pengurus = User::where('role','pengurus')->count();
+        $anggota = User::where('role','anggota')->count();
+        $masyarakat = User::where('role','pengurus')->count();
+        return view('home')->with('title','Home')->with('pengurus',$pengurus)->with('anggota', $anggota)->with('masyarakat', $masyarakat);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,19 +7,19 @@
         <div class="card bg-info text-white col me-2">
             <h5 class="card-header text-white">Pengurus</h5>
             <div class="card-body">
-                Lorem ipsum dolor sit amet.
+                jumlah :{{ $pengurus }}
             </div>
         </div>
         <div class="card bg-success text-white col me-2">
             <h5 class="card-header text-white">Anggota</h5>
             <div class="card-body">
-                Lorem ipsum dolor sit amet.
+                jumlah : {{ $anggota }}
             </div>
         </div>
         <div class="card bg-warning text-white col me-2">
             <h5 class="card-header text-white">Masyarakat</h5>
             <div class="card-body">
-                Lorem ipsum dolor sit amet.
+                jumlah : {{ $masyarakat }}
             </div>
         </div>
     </div>
