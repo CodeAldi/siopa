28c9562a8a340fbbeeab778d81a7599deb23f0e0
CodeAldi <codealdi@gmail.com>
2025-02-15 01:53:29 +0700

    anggota lihat kegiatan done

--- resources/views/pengurus/programKerja.blade.php
+++ resources/views/pengurus/programKerja.blade.php
@@ -28,9 +28,7 @@
                         <th>tanggal kegiatan</th>
                         <th>lokasi kegiatan</th>
                         <th>kategori</th>
-                        @if (Auth()->user()->hasRole('pengurus'))
                         <th>Actions</th>
-                        @endif
                     </tr>
                 </thead>
                 <tbody class="table-border-bottom-0">
@@ -41,7 +39,6 @@
                         <td>{{ $item->tanggal_kegiatan }}</td>
                         <td>{{ $item->lokasi }}</td>
                         <td>{{ $item->kategori }}</td>
-                        @if (Auth()->user()->hasRole('pengurus'))
                         <td class="text-center">
                             <div class="dropdown">
                                 <button type="button" class="btn p-0 dropdown-toggle hide-arrow"
@@ -49,16 +46,21 @@
                                     <i class="bx bx-dots-vertical-rounded"></i>
                                 </button>
                                 <div class="dropdown-menu">
+                                    @if (Auth()->user()->hasRole('pengurus'))
                                     <button type="submit" class="dropdown-item btn btn-success mb-1 text-white" data-bs-toggle="modal"
                                         data-bs-target="#modalEdit" data-index="{{ $item }}" onclick="modalEdit(this)"><i class="bx bx-pencil me-1"></i>
                                         Edit</button>
                                     
                                     <button class="dropdown-item btn btn-danger mb-1 text-white" data-bs-toggle="modal" data-bs-target="#modalDelete"
                                         data-index="{{ $item }}" onclick="modalDelete(this)"><i class="bx bx-x-circle me-1"></i>Hapus</button>
+                                    @else
+                                    <button type="submit" class="dropdown-item btn btn-primary mb-1 text-white" data-bs-toggle="modal"
+                                        data-bs-target="#modalLihat" data-index="{{ $item }}" onclick="modalLihat(this)"><i class="bx bx-show me-1"></i>
+                                        Lihat</button>
+                                    @endif
                                 </div>
                             </div>
                         </td>
-                        @endif
                     </tr>
 
                     @empty
@@ -159,6 +161,86 @@
         </form>
     </div>
 </div>
+<!-- Modal untuk Lihat -->
+<div class="modal fade" id="modalLihat" tabindex="-1" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="modalCreateTitle">Edit Kegiatan Organisasi</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="row">
+                    <div class="col mb-3">
+                        <label for="judul" class="form-label">id</label>
+                        <input type="text" id="Lihatid" class="form-control" name="id" autofocus readonly />
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col mb-3">
+                        <label for="judul" class="form-label">Kegiatan Organisasi</label>
+                        <input type="text" id="Lihatjudul" class="form-control" name="judul" autofocus readonly />
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col mb-3">
+                        <label for="pic" class="form-label">Penanggung Jawab</label>
+                        <select name="penanggung_jawab" id="Lihatpenanggung_jawab" class="form-select" disabled>
+                            <option value="0" hidden> pilih Penanggung jawab kegiatan</option>
+                            @forelse ($penanggungJawab as $item)
+                            <option value="{{ $item->id }}">{{ $item->name }}</option>
+                            @empty
+    
+                            @endforelse
+                        </select>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col mb-3">
+                        <label for="kategori" class="form-label">Kategori Kegiatan</label>
+                        <select name="kategori" id="Lihatkategori" class="form-select" disabled>
+                            <option value="0" hidden> pilih kategori <kegiatan></kegiatan>
+                            </option>
+                            <option value="seni">seni</option>
+                            <option value="rohani">rohani</option>
+                            <option value="budaya">budaya</option>
+                            <option value="sosial">sosial</option>
+                            <option value="olahraga">olahraga</option>
+                            <option value="kejuaraan">kejuaraan</option>
+                            <option value="pendidikan">pendidikan</option>
+                            <option value="kewirausahaan">kewirausahaan</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col mb-3">
+                        <label for="lokasi" class="form-label">lokasi kegiatan</label>
+                        <input type="text" id="Lihatlokasi" class="form-control" name="lokasi" autofocus readonly />
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col mb-3">
+                        <label for="tanggal" class="form-label">tanggal kegiatan</label>
+                        <input type="date" id="Lihattanggal_kegiatan" class="form-control" name="tanggal_kegiatan" readonly />
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col mb-3">
+                        <label for="deskripsi" class="form-label">Isi atau Deskripsi kegiatan</label>
+                        <textarea name="deskrispsi" class="form-control" id="Lihatdeskripsi" cols="30" rows="5" readonly></textarea>
+                    </div>
+                </div>
+    
+    
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
+                        Close
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 <!-- Modal untuk Edit -->
 <div class="modal fade" id="modalEdit" tabindex="-1" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
@@ -173,13 +255,13 @@
                 <div class="row">
                     <div class="col mb-3">
                         <label for="judul" class="form-label">id</label>
-                        <input type="text" id="Editid" class="form-control" name="id" autofocus required />
+                        <input type="text" id="Editid" class="form-control" name="id" autofocus readonly />
                     </div>
                 </div>
                 <div class="row">
                     <div class="col mb-3">
                         <label for="judul" class="form-label">Kegiatan Organisasi</label>
-                        <input type="text" id="Editjudul" class="form-control" name="judul" autofocus required />
+                        <input type="text" id="Editjudul" class="form-control" name="judul" autofocus readonly />
                     </div>
                 </div>
                 <div class="row">
@@ -215,13 +297,13 @@
                 <div class="row">
                     <div class="col mb-3">
                         <label for="lokasi" class="form-label">lokasi kegiatan</label>
-                        <input type="text" id="Editlokasi" class="form-control" name="lokasi" autofocus required />
+                        <input type="text" id="Editlokasi" class="form-control" name="lokasi" autofocus readonly />
                     </div>
                 </div>
                 <div class="row">
                     <div class="col mb-3">
                         <label for="tanggal" class="form-label">tanggal kegiatan</label>
-                        <input type="date" id="Edittanggal_kegiatan" class="form-control" name="tanggal_kegiatan" required />
+                        <input type="date" id="Edittanggal_kegiatan" class="form-control" name="tanggal_kegiatan" readonly />
                     </div>
                 </div>
                 <div class="row">
@@ -308,6 +390,19 @@
         // document.getElementById("HapusNik ").value = myjson.nik;
     console.log(myjson);
     
+    }
+    function modalLihat(item) {
+        let indexnya = item.getAttribute("data-index");
+        const myjsonLihat = JSON.parse(indexnya);
+        // console.log(myjsonLihat);
+        document.getElementById("Lihatid").value = myjsonLihat.id;
+        document.getElementById("Lihatjudul").value = myjsonLihat.judul;
+        document.getElementById("Lihatpenanggung_jawab").value = myjsonLihat.penanggung_jawab;
+        document.getElementById("Lihatkategori").value = myjsonLihat.kategori;
+        document.getElementById("Lihatlokasi").value = myjsonLihat.lokasi;
+        document.getElementById("Lihattanggal_kegiatan").value = myjsonLihat.tanggal_kegiatan;
+        // // document.getElementById("Lihatlama_kegiatan").value = myjsonLihat.lama_kegiatan;
+        document.getElementById("Lihatdeskripsi").value = myjsonLihat.deskrispsi;
     }
     function modalEdit(item) {
         let indexnya = item.getAttribute("data-index");
